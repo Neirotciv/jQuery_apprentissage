4fe258bbb88a7d8527daad64a6a66346eac2325a
VicLam <tanacetifolia@gmail.com>
2022-06-06 08:56:29 +0200

    tests & notes

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # jQuery_apprentissage
+
+### Documentation
+https://api.jquery.com/
+
+### CDN
+https://cdnjs.com/libraries/jquery
+
+---
+
+### Sélectionner un élément
+```javascript
+document.querySelector("h1");
+// jQuery
+$("h1");
+
+document.querySelectorAll("h1");
+// jQuery
+$("h1");
+```
+
+### Propriété css
+```javascript
+// Set color
+$("h1").css("color", "green");
+// Get color, 1 paramètre
+$("h1").css("color");
+```
+
+### Manipuler du texte
+```javascript
+$("h1").text("Mon nouveau texte");
+// innerHTML
+$("h1").html("<em>Titre en italique</em> ");
+```
+
+### Modifier des attributs
+```javascript
+// Set attribute
+$("a").attr("href", "http://www.tezos.com");
+// Get attribute
+```
+
+### Ajouter, supprimer des éléments
+```javascript
+// Dans un élément à la fin
+$("h1").append();
+// Dans un élément au début
+$(".button-wrapper").prepend("<p>Au début</p>");
+// Après un élément
+$("h1").after("<h3>Subtitle</h3>");
+// Avant
+$("h1").before();
+```
+
+### Animation
+https://api.jquery.com/category/effects/
--- index.js
+++ index.js
@@ -16,3 +16,42 @@ $("h1").addClass("big-title margin-50");
 
 // Supprimer une classe
 $("h1").removeClass("big-title");
+
+// Modifier le texte et html des boutons, enfant du wrapper
+$(".button-wrapper > button").text("Don't click me")
+// $("button").html("<em>In italic</em>")
+
+// Attribute, href, class...
+$("a").attr("href", "http://www.tezos.com");
+
+// Event listener
+$("h1").click(() => {
+    $("h1").css("color", "blue");
+    $("h1").addClass("big-title");
+})
+
+$(".button-wrapper > button").click(() => {
+    $("h1").css("color", "yellow");
+});
+
+$("input").keypress((event) => {
+    $("#input-writing").text(event.key);
+});
+
+$("input").on("mouseover", () => {
+    $("input").css("backgroundColor", "red");
+})
+
+// Ajouter à l'intérieur et à la suite d'autres éléments
+$(".wrapper").append("<p>Je suis dans le wrapper</p>");
+// Dans un élément au début
+$(".button-wrapper").prepend("<p>Au début</p>");
+// Ajouter après
+$("h1").after("<h3>Subtitle</h3>")
+// Ajouter avant
+$("h1").before("bonjour");
+
+// Animation
+$("#animate-button").click(() => {
+    $(".animate-wrapper > h3").animate({ fontSize: "50px" }, 1500).fadeToggle();
+})
